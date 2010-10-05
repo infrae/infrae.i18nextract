d9b1a27e328aca3d60d8f9443bf1b56c1f06c2c0
Sylvain Viollon <thefunny@gmail.com>
2010-10-05 16:29:09 +0000

    Add an option to directly compile files.

--- src/infrae/i18nextract/merge.py
+++ src/infrae/i18nextract/merge.py
@@ -3,14 +3,13 @@
 # $Id$
 
 from optparse import OptionParser
-import sys
 import os
 import re
 
 from zope.configuration.name import resolve
 
 
-def merge_files(path):
+def merge_files(path, compile_translations=False):
     po_file_reg = re.compile('(.*)-([a-z]{2})\.po$')
     for filename in os.listdir(path):
         filename = os.path.join(path, filename)
@@ -19,34 +18,32 @@ def merge_files(path):
             if not match:
                 # not a .po file
                 continue
-            language = match.group(2)
-            if language == 'en':
-                # we assume english is default, so bail out
-                continue
             domain = match.group(1)
+            language = match.group(2)
             pot_path = os.path.join(path, '%s.pot' % domain)
             print 'Merging language "%s", domain "%s"' % (language, domain)
             os.system('msgmerge -N -U %s %s' %(filename, pot_path))
+            if compile_translations:
+                compiled_filename = os.path.splitext(filename)[0] + '.mo'
+                os.system('msgfmt -o %s %s' % (compiled_filename, filename))
 
     for language in os.listdir(path):
         lc_messages_path = os.path.join(path, language, 'LC_MESSAGES')
-
-        # English is the default for Zope, so ignore it
-        if language == 'en':
-            continue
 
         # Make sure we got a language directory
         if not os.path.isdir(lc_messages_path):
             continue
 
-        msgs = []
-        for domain_file in os.listdir(lc_messages_path):
-            if domain_file.endswith('.po'):
-                domain_path = os.path.join(lc_messages_path, domain_file)
-                pot_path = os.path.join(path, domain_file+'t')
-                domain = domain_file.split('.')[0]
-                print 'Merging language "%s", domain "%s"' %(language, domain)
-                os.system('msgmerge -N -U %s %s' %(domain_path, pot_path))
+        for filename in os.listdir(lc_messages_path):
+            if filename.endswith('.po'):
+                pot_path = os.path.join(path, filename + 't')
+                domain = '.'.join(filename.split('.')[:-1])
+                filename = os.path.join(lc_messages_path, filename)
+                print 'Merging language "%s", domain "%s"' % (language, domain)
+                os.system('msgmerge -N -U %s %s' %(filename, pot_path))
+                if compile_translations:
+                    compiled_filename = os.path.splitext(filename)[0] + '.mo'
+                    os.system('msgfmt -o %s %s' % (compiled_filename, filename))
 
 
 def merge(output_package):
@@ -57,17 +54,20 @@ def merge(output_package):
     parser.add_option(
         "-p", "--path", dest="path",
         help="path where the translation to merge are")
+    parser.add_option(
+        "-c", "--compile", dest="compile", action="store_true",
+        help="compile translations as well")
     (options, args) = parser.parse_args()
 
     if options.path:
-        merge_files(options.path)
+        merge_files(options.path, options.compile)
     else:
         python_package = resolve(output_package)
         path = os.path.dirname(python_package.__file__)
         i18n_path = os.path.join(path, 'i18n')
         if os.path.isdir(i18n_path):
             print "Merging package %s/i18n..." % output_package
-            merge_files(i18n_path)
+            merge_files(i18n_path, options.compile)
 
 
 def egg_entry_point(kwargs):
