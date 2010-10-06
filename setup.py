46f813bf5c674c8492e55ff9e849910ef16a9874
Sylvain Viollon <thefunny@gmail.com>
2010-10-06 09:34:50 +0000

    prepare to tag.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ name = 'infrae.i18nextract'
 
 setup(
     name = name,
-    version='1.2dev',
+    version='1.2',
     author='Sylvain Viollon',
     author_email='info@infrae.com',
     description='Buildout recipe to extract i18n files in Silva',
